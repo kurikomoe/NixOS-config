91aaf35b8681e1109cda543e0b3443a8fcb16954
KurikoMoe <kurikomoe@gmail.com>
2024-10-08 17:22:00 +0800

    update(python): update python version

--- home-manager/packages/devs/langs/python.nix
+++ home-manager/packages/devs/langs/python.nix
@@ -4,17 +4,17 @@ let
 
 in {
   # use latest python
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     python3 = repos.pkgs-unstable.python3;
-  #   })
-  # ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      # python3 = repos.pkgs-unstable.python312.override {
+      #   enableGIL = false;
+      # };
+      python3 = repos.pkgs-unstable.python3;
+    })
+  ];
 
   home.packages = with pkgs; [
     pipx
-
-    python312
-
-    python312Packages.pysocks
+    python3
   ];
 }
