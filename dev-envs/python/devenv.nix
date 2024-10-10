c870936ab62a8893b276881d3ed6547b30062444
KurikoMoe <kurikomoe@gmail.com>
2024-10-10 23:24:15 +0800

    update(devenv): update python devenv template

--- dev-envs/python/devenv.nix
+++ dev-envs/python/devenv.nix
@@ -13,18 +13,6 @@
   };
 
   nixConfig = {
-    substituters = [
-      https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store
-      https://mirrors.ustc.edu.cn/nix-channels/store
-      https://mirror.sjtu.edu.cn/nix-channels/store
-      https://cache.nixos.org
-      https://nix-community.cachix.org
-    ];
-    trusted-substituters = substituters;
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
     extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
@@ -70,15 +58,16 @@
 
                 enterShell = ''
                   export BLUEARCHIVE=true;
+                  export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/"
                 '';
 
                 languages.python = {
                   enable = true;
-                  version = "3.12";
+                  package = pkgs.python3;
                   poetry = {
                     enable = true;
-                    activate.enable = false;
-                    install.enable = false;
+                    activate.enable = true;
+                    install.enable = true;
                   };
                 };
               }
