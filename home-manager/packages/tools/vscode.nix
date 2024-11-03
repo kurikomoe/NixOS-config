4d53f5518893fe6d755b5f57f3b8a1e4cf481653
KurikoMoe <kurikomoe@gmail.com>
2024-11-03 22:43:10 +0800

    update(vscode): add devcontainer

--- home-manager/packages/tools/vscode.nix
+++ home-manager/packages/tools/vscode.nix
@@ -1,8 +1,12 @@
-p@{ inputs, ... }:
+p@{ inputs, pkgs, ... }:
 
 {
   imports = [
     "${inputs.nixos-vscode-server}/modules/vscode-server/home.nix"
+  ];
+
+  home.packages = with pkgs; [
+    devcontainer
   ];
 
   services.vscode-server = {
