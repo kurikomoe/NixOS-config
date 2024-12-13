aa660c668995f76b92f99ab27193f403f2ac76c6
KurikoMoe <kurikomoe@gmail.com>
2024-12-13 17:47:17 +0800

    fix(dotnet): re-enable dotnet runtimes
    update locks

--- home-manager/pkgs/libs/dotnet.nix
+++ home-manager/pkgs/libs/dotnet.nix
@@ -3,20 +3,42 @@
   lib,
   ...
 }: let
-  combined-runtime-pkgs = with pkgs;
-  with dotnetCorePackages;
-    combinePackages [
-      dotnet_9.runtime
-      dotnet_8.runtime
-      # dotnet-runtime_7
-      dotnet-runtime
+  # combined-runtime-pkgs = with pkgs;
+  # with dotnetCorePackages;
+  #   combinePackages [
+  #     dotnet_9.runtime
+  #     dotnet_8.runtime
+  #     # dotnet-runtime_7
+  #     dotnet-runtime
+  #   ];
+  combinedDotnetRuntimes = pkgs.symlinkJoin {
+    name = "dotnet-runtimes";
+    paths = with pkgs;
+    with dotnetCorePackages; [
+      dotnet-runtime_9
+      dotnet-runtime # 8 at current time
+      dotnet-runtime_6
     ];
+    symlinkJoinBuildInputs = [
+      # Avoid conflicts by preferring the first occurrence of each file
+      "--resolve-conflicts=preferLeft"
+    ];
+  };
 in {
-  home.packages = with pkgs; [
-    # (lib.lowPrio combined-runtime-pkgs)
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-runtime-wrapped-6.0.36"
+    "dotnet-runtime-6.0.36"
+    "dotnet-sdk-wrapped-6.0.136"
+    "dotnet-sdk-6.0.136"
+  ];
+
+  home.packages = with pkgs;
+  with dotnetCorePackages; [
+    # combined-runtime-pkgs
+    combinedDotnetRuntimes
   ];
 
   home.sessionVariables = {
-    # DOTNET_ROOT = lib.mkDefault "${combined-runtime-pkgs}/share/dotnet";
+    DOTNET_ROOT = lib.mkDefault "${combinedDotnetRuntimes}/share/dotnet";
   };
 }
