63133b89eae545af47041e8c84f2d4e61f987e29
KurikoMoe <kurikomoe@gmail.com>
2025-03-02 17:25:06 +0800

    add(devenvs): add kotlin/java devenv
    update locks
    add kotlin/java devenv

---
+++ devenvs/kotlin/flake.nix
@@ -0,0 +1,110 @@
+{
+  description = "Kuriko's Java/Kotlin Template";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs-python = {
+      url = "github:cachix/nixpkgs-python";
+      inputs = {nixpkgs.follows = "nixpkgs";};
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        system,
+        lib,
+        ...
+      }: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [];
+        };
+
+        jdkCustom = pkgs.jdk17;
+        kotlinCustom = pkgs.kotlin.override {jre = jdkCustom;};
+      in {
+        formatter = pkgs.alejandra;
+
+        devenv.shells.default = {
+          # Enable this to avoid forced -O2
+          # hardeningDisable = [ "all" ];
+
+          packages = with pkgs; [
+            # tools
+            just
+            hello
+
+            gradle
+            jdkCustom
+            kotlinCustom
+          ];
+
+          languages = {
+            kotlin.enable = true;
+
+            java.enable = true;
+            java.gradle.enable = true;
+            java.maven.enable = false;
+
+            python = {
+              enable = false;
+              # package = pkgs.python312;
+              # version = "3.12";
+              uv.enable = true;
+            };
+          };
+
+          pre-commit = {
+            addGcRoot = true;
+            hooks = {
+              alejandra.enable = true;
+              # Python
+              isort.enable = true;
+              mypy.enable = true;
+              pylint.enable = true;
+              flake8.enable = true;
+            };
+
+            # Check Secrets
+            trufflehog = {
+              enable = true;
+              entry = let
+                script = pkgs.writeShellScript "precommit-trufflehog" ''
+                  set -e
+                  ${pkgs.trufflehog}/bin/trufflehog --no-update git "file://$(git rev-parse --show-toplevel)" --since-commit HEAD --results=verified --fail
+                '';
+              in
+                builtins.toString script;
+            };
+          };
+
+          cachix.pull = ["devenv"];
+          cachix.push = "kurikomoe";
+        };
+      };
+    };
+}
