bb527881ec964cdf58e3325480261145d9eda0be
KurikoMoe <kurikomoe@gmail.com>
2025-03-06 15:42:12 +0800

    update locks

--- devices/KurikoG14/nixos.nix
+++ devices/KurikoG14/nixos.nix
@@ -88,12 +88,12 @@
 
         networking.hostName = hostName;
 
-        i18n.supportedLocales = [
-          "en_US.UTF-8/UTF-8"
-          "zh_CN.UTF-8/UTF-8"
-          "ja_JP.UTF-8/UTF-8"
-        ];
-        i18n.defaultLocale = "en_US.UTF-8/UTF-8";
+        # i18n.supportedLocales = [
+        #   "en_US.UTF-8/UTF-8"
+        #   "zh_CN.UTF-8/UTF-8"
+        #   "ja_JP.UTF-8/UTF-8"
+        # ];
+        # i18n.defaultLocale = "en_US.UTF-8/UTF-8";
 
         fonts.fontDir.enable = true;
         fonts.packages = with pkgs; [
--- nixos/template.nix
+++ nixos/template.nix
@@ -71,12 +71,12 @@ in
             inputs.nix-alien.packages.${system}.nix-alien
           ];
 
-          i18n.supportedLocales = [
-            "en_US.UTF-8/UTF-8"
-            "zh_CN.UTF-8/UTF-8"
-            "ja_JP.UTF-8/UTF-8"
-          ];
-          i18n.defaultLocale = "en_US.UTF-8/UTF-8";
+          # i18n.supportedLocales = [
+          #   "en_US.UTF-8/UTF-8"
+          #   "zh_CN.UTF-8/UTF-8"
+          #   "ja_JP.UTF-8/UTF-8"
+          # ];
+          # i18n.defaultLocale = "en_US.UTF-8/UTF-8";
 
           programs = {
             nix-ld = {
