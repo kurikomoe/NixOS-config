d99eace04e07187b96aca1c0d19bdbba92e08430
KurikoMoe <kurikomoe@gmail.com>
2025-03-18 18:34:41 +0800

    update python packages

--- home-manager/pkgs/devs/langs/python.nix
+++ home-manager/pkgs/devs/langs/python.nix
@@ -21,27 +21,64 @@ in {
 
     (python3.withPackages (py-pkgs:
       with py-pkgs; [
-        pexpect
-
-        xlsxwriter
-        python-docx
+        # 各种配置文件格式
+        lxml
         pyyaml
 
-        pytz
+        # 异步
+        # asyncio
+
+        # MS office
+        xlsxwriter
+        python-docx
+        openpyxl
+
+        # 函数式编程库
+        toolz
         more-itertools
 
+        # 时间相关的库
+        pytz
+        pendulum
+
+        # 日志相关的库
+        loguru
         coloredlogs
 
+        # Database
+        pymysql
+        pymongo
+        # sqlite3
+        ## ORM
+        sqlalchemy
+
+        # 图像处理的库
         pillow
-        pandas
-        numpy
+        opencv
         seaborn
         matplotlib
+
+        # 音频处理
+        # librosa
+
+        # 数据处理的库
+        pandas
+        numpy
         scipy
+
+        # 进度显示
         tqdm
 
+        # AI 相关的库
+        # (callPackages torch {
+        #   cudaPackages.cudaVersion = "12.1";
+        # })
+        # torchvision
+
+        # 数据格式
         pydantic
 
+        #网络
         pysocks
         aiohttp
         fastapi
@@ -49,6 +86,22 @@ in {
         uvicorn
         requests
         beautifulsoup4
+        scrapy
+        flask
+
+        openapi-python-client
+
+        # GUI
+        pyqt5
+        pyqt6
+
+        # Terminal 工具
+        pexpect
+
+        # 其他
+        setuptools
+        # ninja
+        scons
       ]))
   ];
 }
