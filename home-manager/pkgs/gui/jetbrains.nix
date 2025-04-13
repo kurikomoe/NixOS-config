50c310fb4b3b21bbb4a2fd31f696f549858aca0f
KurikoMoe <kurikomoe@gmail.com>
2025-04-13 22:13:48 +0800

    fix jetbrains-remote

--- home-manager/pkgs/gui/jetbrains.nix
+++ home-manager/pkgs/gui/jetbrains.nix
@@ -1,5 +1,17 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  root,
+  ...
+}: let
 in {
+  disabledModules = [
+    "programs/jetbrains-remote.nix"
+  ];
+
+  imports = [
+    "${root.pkgs}/home-manager/jetbrains-remote.nix"
+  ];
+
   home.packages = with pkgs; [
     jetbrains-toolbox
   ];
